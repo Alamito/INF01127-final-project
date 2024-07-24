3fb63df9600b6f0fedde766c1ae75ba3d938d19f
Bruno Costa <brunocosta0427@gmail.com>
2024-07-23 21:52:34 -0300

    login page redone

--- src/view/pages/log-in/log-in.tsx
+++ src/view/pages/log-in/log-in.tsx
@@ -3,6 +3,7 @@ import './log-in.css';
 import React, { useState } from 'react';
 import { CiLogin } from "react-icons/ci";
 import { signIn } from 'next-auth/react';
+import { Button, Card, CardBody, Col, Container, FloatingLabel, Form, Row } from 'react-bootstrap';
 
 export const LogIn = () => {
     const [formData, setFormData] = useState({
@@ -24,19 +25,38 @@ export const LogIn = () => {
         });
     }
 
-
     return (
-        <div className="container-log-in">
-            <input placeholder="Email" name="email" className="input-style" type="text" onChange={handleInputChange}></input>
-            <input placeholder="Password" name="password" className="input-style" type="password" onChange={handleInputChange}></input>
+    <Container className="content">
+        <Row>
+            <Col xs={8}>
 
-            <div className='actions'>
-            <button className="button" onClick={handleSubmit}>
-                <CiLogin />
-                <p className="text">Sign Up</p>
-            </button>
-            </div>
+            </Col>
+            <Col xs={4}>
+                <Card style={{ marginTop: '50%', borderRadius: '15px', padding:'20px' }}>
+                    <CardBody>
+                        <h1 className='display-3 m-4'>Entrar</h1>
+                        <FloatingLabel
+                            controlId="floatingInput"
+                            label="E-mail"
+                            className="mb-3 mt-5">
+                            <Form.Control type="email" placeholder="name@example.com" onChange={handleInputChange}/>
+                        </FloatingLabel>
+                        <FloatingLabel controlId="floatingPassword" label="Senha">
+                            <Form.Control type="password" placeholder="Senha" onChange={handleInputChange}/>
+                        </FloatingLabel>
 
-        </div>
+                        <div className="d-flex flex-column m-5">
+                            <Button variant="success" size="lg" className="mb-2" onClick={handleSubmit}>
+                                Acessar
+                            </Button>
+                            <Button variant="link" className="p-0">
+                                Cadastrar-se
+                            </Button>
+                        </div>
+                    </CardBody>
+                </Card>
+            </Col>
+        </Row>
+    </Container>
     );
 };
